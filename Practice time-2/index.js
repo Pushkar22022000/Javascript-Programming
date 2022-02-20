d720eb789ac125226d746845acaccff317c8695d
Pushkar22022000 <pushkar.kamble@konverge.ai>
2022-02-21 03:13:20 +0530

    javascript practice 2 index.js file added

---
+++ Practice time-2/index.js
@@ -0,0 +1,9 @@
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.txtContent = "something went wrong,please check"
+    
+
+}
